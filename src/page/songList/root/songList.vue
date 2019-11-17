2effcc45ba81460458c338f88790fb1a4410e9b1
YuanZheng <992832548@qq.com>
2019-11-17 10:00:10 +0800

    将侧边栏适配页面

--- src/page/songList/root/songList.vue
+++ src/page/songList/root/songList.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="page" id="songList">
-    歌单{{id}}
+    <main class="songlistMain">
+      歌单{{id}}
+    </main>
+    <pageAside kind="songList"/>
   </div>
 </template>
 
 <script>
+import pageAside from './../../../components/pageAside/pageAside';
 export default {
+  components: {
+    pageAside
+  },
   props: {
     id: ''
+  },
+  methods: {
+    rememberId(){
+      this.$store.commit("aside/setSongListId", this.id);
+    }
+  },
+  mounted(){
+    this.rememberId();
+  },
+  updated(){
+    this.rememberId();
   }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+#songList{
+  border-left: 1px solid #E9E9E9;
+  border-right: 1px solid #E9E9E9;
+  background-color: #fff;
+  overflow: hidden;
+  .songlistMain{
+    width: 709px;
+    float: left;
+    border-right: 1px solid #E9E9E9;
+  }
+}
 
 </style>
